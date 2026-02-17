bfd5e622f9bb6d8becc106f6a5a19268d1ff89e7
Mark <ruisdaeless@yahoo.com>
2026-02-17 23:01:00 +0400

    Initial Commit

---
+++ poab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  poab
+//
+//  Created by Mark on 17/02/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
